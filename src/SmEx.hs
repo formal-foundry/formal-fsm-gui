cbac101a20feb6367d2cd4ff9201fe2ce6738c15
nieradkokrystian <krystian.nieradko@gmail.com>
2023-10-23 21:56:54 +0200

    WIP

--- src/SmEx.hs
+++ src/SmEx.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# OPTIONS_GHC -w #-}
 
 module SmEx where
 
@@ -9,54 +10,43 @@ import Data.Aeson.KeyMap as AK
 import Data.Aeson.Key as K
 import Data.Aeson.Types as T
 import Data.ByteString.Lazy as BSL
-import Data.Map
+import Data.Map as M
+import Data.Text as T
 
--- data Person = Person {
---       name :: String
---     , age  :: Int
---     } deriving Show
+import Types
 
--- instance FromJSON Person where
---     parseJSON = withObject "Person" $ \v -> Person
---         <$> v .: "name"
---         <*> v .: "age"
 
-data Transition s  = Transition {target :: s  }
 
-data State s t =  State { transitions :: Map t (Transition s)
-                         }
-
-data FSM s t = FSM { states :: Map s (State s t)
-                    }
 
 ex1 :: ByteString
 ex1  = [i| {
   "id": "test1",
   "initial": "State 1",
   "states": {
-    "State 1": {
+
+"State 1": {
       "on": {
-        "1-2 action": {
-          "target": "State 3"
+        "1-3 action": {
+        "target": "State 3"
         },
-        "1 -1 action": {
+        "1-1 action": {
           "internal": true
         }
       }
     },
     "State 3": {
-      "on": {
-        "2-3 action": {
+      "on":  {
+        "3-2 action": {
           "target": "State 2"
         },
-        "2-1 action": {
+        "3-1 action": {
           "target": "State 1"
         }
       }
     },
     "State 2": {
       "on": {
-        "3-4 action": {
+        "2-4 action": {
           "target": "State 4"
         }
       }
@@ -70,9 +60,68 @@ ex1  = [i| {
           "internal": true
         }
       }
-    }
+
+}
   }
 }
 |]
+
+x :: Value -> String
+x val=  case val of
+    Object z -> case AK.lookup (K.fromString "id") z of
+      Just e -> case e of
+        String s -> T.unpack s
+        _ -> "empty_schema_name"
+    _ ->  "empty_schema_name"
+
+getStates :: ByteString -> Value
+getStates j = undefined 
+
+convertInternal :: Value -> Value
+convertInternal old =
+  case old of
+    Object o ->
+      let 
+        states = case AK.lookup (K.fromString "states") o of
+          Nothing -> String "Bad Val"
+          Just stat -> case stat of
+                         Object s -> let z = AK.map id s
+                                     in Object z
+                         _ -> String "Bad Val"
+      in
+       String "Bad Val"
+    _ -> String "f"  
+
+
+genFSM :: ByteString -> Maybe (FSM s t)
+genFSM j =
+  case decode j :: Maybe Value of
+    Nothing -> Nothing
+    Just v -> 
+           let nv = convertInternal v
+               name =  x v
+               states = "f"
+               newJ = convertInternal v
+            in
+             Just  FSM {states = M.empty
+                 , name = name
+                 , initial = State {transitions = M.empty}
+
+                 }
+
+
+-- data Transition s  = Transition {target :: s  }
+
+
+
+
+-- d ata State s t =  State { transitions :: Map t (Transition s)
+--                          }
+
+-- data FSM s t = FSM { states :: Map s (State s t)
+--                    , name :: String
+--                    , initial :: State s t 
+--                    }
+
 
 
---
+++ src/Types.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# OPTIONS_GHC -w #-}
+
+module Types where
+
+import Data.Map
+
+
+data Transition s  = Transition {target :: s  }
+
+data State s t =  State { transitions :: Map t (Transition s)
+                         }
+
+data FSM s t = FSM { states :: Map s (State s t)
+                   , name :: String
+                   , initial :: State s t 
+                   }
