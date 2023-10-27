8ebeda757c6ceceb4ec491339182e524c355069b
nieradkokrystian <krystian.nieradko@gmail.com>
2023-10-27 23:19:11 +0200

    agda Creat

--- src/SmEx.hs
+++ src/SmEx.hs
@@ -14,9 +14,10 @@ import Data.Aeson.Types as T
 import Data.ByteString.Lazy as BSL
 import Data.Map as M
 import Data.Text as T
-
+import Data.String
+import Data.List as L
+import Data.Char as C
 import Types
-
 
 
 
@@ -68,14 +69,22 @@ ex1  = [i| {
 }
 |]
 
--- data Transition s  = Transition {target :: s  }
 
--- data State s t =  State { transitions :: Map t (Transition s)
---                          }
--- data FSM s t = FSM { states :: Map s (State s t)
---                    , name :: String
---                    , initial :: State s t 
---                    }
+createA :: IO ()
+createA = do
+  let fsm  = createFSM ex1
+      stateL = L.reverse $ L.map sc $ M.keys $ states fsm
+  putStrLn $ "module Problem where\n\n"
+    ++ "data State : Set where\n"
+    ++ "  " ++ L.foldl1 (\x a->  a ++ " " ++ x  ) stateL ++ " : State\n\n"
+    ++"data Input : Set where\n" 
+
+sc :: [Char] -> String
+sc (s:xs) = case C.isDigit s of
+  False -> case C.isLower s of
+    True -> L.filter (not . isSpace) (s:xs)
+    False -> L.filter (not . isSpace)((C.toLower s):xs)
+  True -> L.filter (not . isSpace) (s:xs)
 
 
 createFSM :: ByteString -> FSM String String
@@ -117,57 +126,5 @@ createTransition k v = case v of
 ----------TESTSc------------
 test :: Int
 test  = M.size $ states $ createFSM ex1
-
--- test2 :: int
--- test2 = case  decode  json :: maybe value of
---   nothing -> m.size fsm m.empty
---   just val -> case val of
---     object o ->
---       let 
---         states = case ak.lookup (k.fromstring "states") o of
---           nothing -> m.size fsm m.empty
---           just s -> case s of
---             object x -> m.size fsm $ m.map createstate (tostringfromkm x)
-
---     _ -> 0
-
-
-            
-------------------------------------
--- createFSM :: ByteString -> FSM s t
--- createFSM json = case genValue json of
---     Object o ->
---       let 
---         states = case AK.lookup (K.fromString "states") o of
---           Nothing -> FSM M.empty
---           Just s -> case s of
---             Object x -> FSM $ M.map createState (toStringFromKM x)
---             _ -> FSM M.empty
---       in 
---        FSM M.empty
---     _ -> FSM M.empty
-
-
--- toStringFromKM :: KeyMap Value -> Map String Value
--- toStringFromKM km = M.mapKeys (\x -> K.toString x) (AK.toMap km)
- 
-
--- createState :: Value -> State s t
--- createState v = case v of
---   Object o ->  case AK.lookup (K.fromString "on") o of
---      Just on -> case on of
---        Object s -> State M.empty
---      Nothing  -> State M.empty
---   _ -> State M.empty
-
--- createTransition :: forall s. Value -> Transition s
--- createTransition v = Transition {target = "State 3"}
-
-
--- genValue :: ByteString -> Value
--- genValue j =
---   case decode j :: Maybe Value of
---     Nothing -> Null
---     Just v -> v
 
 
